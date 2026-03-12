5c4319a194f81ce7d60451381bbeb2c0589bbff5
IT24103661 <IT24103661@my.sliit.lk>
2026-03-12 17:10:17 +0530

    add register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+
+const AUTH_ROUTES = ['/login', '/register'];
+
+function Layout() {
+  const { pathname } = useLocation();
+  const isAuth = AUTH_ROUTES.includes(pathname);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {!isAuth && <Navbar />}
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          {/* Role-based dashboards will be added here */}
+        </Routes>
+      </main>
+      {!isAuth && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            {/* Additional role-based routes will be added here */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Layout />
     </BrowserRouter>
   );
 }
